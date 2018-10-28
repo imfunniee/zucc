128532c4cfa921a56eaed6ac808473c3b7f1b0c6
imfunny <36105478+imfunniee@users.noreply.github.com>
2018-10-28 15:33:53 +0530

    Add files via upload
    still working on it

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const zucc = require('say');
+var express = require('express');
+var app = express();
+var port = process.env.port || process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
 
-zucc.speak("Hello", 1.40, (err) => {
-    if (err) {
-      return console.error(err)
-    }
-    console.log('Text has been spoken.')
-  });
+zucc.speak(`zucc is serving on port ${port}!!`, 1.34, (err) => {
+  if (err) { return console.error(err); }
+});
+
+app.get('/', function(req, res) {
+res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => console.log(`zucc is serving on port ${port}!`));
